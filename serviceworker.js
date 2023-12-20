fdea93fa3145315b8e035bb9b53451446a2bc20f
Matthew Kriel <matthew.k@skrmiish.com>
2023-12-20 12:31:08 +0200

    index.html done

---
+++ serviceworker.js
@@ -0,0 +1,70 @@
+// Cache version control
+const CACHE_VERSION = 7;
+const CURRENT_CACHE = `cache-storage-V${CACHE_VERSION}`;
+
+
+// List of routes that are going to be cached
+const cacheFiles = [
+    // pages
+    '/index.html',
+    // files
+    '/static/style.css',
+    '/static/img/app-icon.png',
+    '/static/img/festival-map.jpg',
+    '/static/js/idbop.js',
+    '/static/patua-one-v20-latin-regular.woff2',
+]
+
+
+// activation event: clean up previously registered service workers
+self.addEventListener('activate', event =>
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  )
+);
+
+
+// Install event: cache the files
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CURRENT_CACHE).then(function(cache) {
+        return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith((async() => {
+
+    const cache = await caches.open(CURRENT_CACHE);
+
+    try {
+        const cachedResponse = await cache.match(event.request);
+        if(cachedResponse) {
+            console.log('cachedResponse: ', event.request.url);
+            return cachedResponse;
+        }
+
+        const fetchResponse = await fetch(event.request);
+        if(fetchResponse) {
+            console.log('fetchResponse: ', event.request.url);
+            await cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+        }
+    }   catch (error) {
+        console.log('Fetch failed: ', error);
+        const cachedResponse = await cache.match('/en/offline.html');
+        return cachedResponse;
+    }
+  })());
+
+});
---
+++ static/js/idbop.js
@@ -0,0 +1,356 @@
+const lineup = [
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 1,
+    "fields": {
+      "artist_name": "Almero",
+      "genre": "Afro",
+      "start_time": "16:00",
+      "end_time": "18:00",
+      "day_of_week": 0,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 2,
+    "fields": {
+      "artist_name": "Robbie Dinnie",
+      "genre": "Indie Dance",
+      "start_time": "18:00",
+      "end_time": "19:30",
+      "day_of_week": 0,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 3,
+    "fields": {
+      "artist_name": "Shmikesss",
+      "genre": "Indie Dance",
+      "start_time": "19:30",
+      "end_time": "21:00",
+      "day_of_week": 0,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 4,
+    "fields": {
+      "artist_name": "Audiojerk",
+      "genre": "Techno",
+      "start_time": "21:00",
+      "end_time": "23:00",
+      "day_of_week": 0,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 5,
+    "fields": {
+      "artist_name": "Ivan Turanjanin",
+      "genre": "Techno",
+      "start_time": "23:00",
+      "end_time": "01:00",
+      "day_of_week": 0,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 6,
+    "fields": {
+      "artist_name": "Todd Duff",
+      "genre": "Psytech",
+      "start_time": "01:00",
+      "end_time": "03:00",
+      "day_of_week": 0,
+      "time_period": 3
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 7,
+    "fields": {
+      "artist_name": "Yoga w/ Eva",
+      "genre": "Lifestyle",
+      "start_time": "08:00",
+      "end_time": "09:00",
+      "day_of_week": 1,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 8,
+    "fields": {
+      "artist_name": "Hoop Class w/ Tracy",
+      "genre": "Lifestyle",
+      "start_time": "09:00",
+      "end_time": "10:00",
+      "day_of_week": 1,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 9,
+    "fields": {
+      "artist_name": "Amith",
+      "genre": "House",
+      "start_time": "10:00",
+      "end_time": "12:00",
+      "day_of_week": 1,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 10,
+    "fields": {
+      "artist_name": "Mahandana",
+      "genre": "Afro",
+      "start_time": "12:00",
+      "end_time": "13:30",
+      "day_of_week": 1,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 11,
+    "fields": {
+      "artist_name": "Sides",
+      "genre": "House",
+      "start_time": "13:30",
+      "end_time": "15:00",
+      "day_of_week": 1,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 12,
+    "fields": {
+      "artist_name": "Sebby",
+      "genre": "Indie Dance",
+      "start_time": "15:00",
+      "end_time": "16:30",
+      "day_of_week": 1,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 13,
+    "fields": {
+      "artist_name": "Steph Vass",
+      "genre": "Indie Dance",
+      "start_time": "16:30",
+      "end_time": "18:00",
+      "day_of_week": 1,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 14,
+    "fields": {
+      "artist_name": "Karoo",
+      "genre": "Indie Dance",
+      "start_time": "18:00",
+      "end_time": "20:00",
+      "day_of_week": 1,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 15,
+    "fields": {
+      "artist_name": "Sonic Bloom",
+      "genre": "Techno",
+      "start_time": "20:00",
+      "end_time": "21:30",
+      "day_of_week": 1,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 16,
+    "fields": {
+      "artist_name": "Ryan Hill",
+      "genre": "Techno",
+      "start_time": "21:30",
+      "end_time": "23:30",
+      "day_of_week": 1,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 17,
+    "fields": {
+      "artist_name": "Lewis",
+      "genre": "Techno",
+      "start_time": "23:30",
+      "end_time": "01:30",
+      "day_of_week": 1,
+      "time_period": 2
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 18,
+    "fields": {
+      "artist_name": "Rabbit",
+      "genre": "Psytech",
+      "start_time": "01:30",
+      "end_time": "03:00",
+      "day_of_week": 1,
+      "time_period": 3
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 19,
+    "fields": {
+      "artist_name": "Portal",
+      "genre": "Progressive Psy",
+      "start_time": "03:00",
+      "end_time": "05:00",
+      "day_of_week": 1,
+      "time_period": 3
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 20,
+    "fields": {
+      "artist_name": "Yoga w/ Ashleigh",
+      "genre": "Lifestyle",
+      "start_time": "08:00",
+      "end_time": "09:00",
+      "day_of_week": 2,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 21,
+    "fields": {
+      "artist_name": "Josh Mac",
+      "genre": "Progressive Psy",
+      "start_time": "09:00",
+      "end_time": "11:00",
+      "day_of_week": 2,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 22,
+    "fields": {
+      "artist_name": "Tall Tales",
+      "genre": "Progressive Psy",
+      "start_time": "11:00",
+      "end_time": "12:00",
+      "day_of_week": 2,
+      "time_period": 0
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 23,
+    "fields": {
+      "artist_name": "Geometric Flux",
+      "genre": "Progressive Psy",
+      "start_time": "12:00",
+      "end_time": "13:00",
+      "day_of_week": 2,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 24,
+    "fields": {
+      "artist_name": "Freedom Fighters",
+      "genre": "Progressive Psy",
+      "start_time": "13:00",
+      "end_time": "16:00",
+      "day_of_week": 2,
+      "time_period": 1
+    }
+  },
+  {
+    "model": "habitat_pwa.artistlineup",
+    "pk": 25,
+    "fields": {
+      "artist_name": "Breaker",
+      "genre": "Progressive Psy",
+      "start_time": "16:00",
+      "end_time": "17:00",
+      "day_of_week": 2,
+      "time_period": 1
+    }
+  }
+]
+
+
+const scheduleList = document.querySelector('.schedule-list');
+const filterButton = document.querySelectorAll('.filter-button');
+const filterButtons = document.querySelectorAll('.day-selector *');
+const navFilterButton = document.querySelectorAll('.nav-item');
+const navFilterButtons = document.querySelectorAll('.navbar *');
+
+
+//
+window.addEventListener('DOMContentLoaded', function () {
+    let startDay = lineup.filter(function (lineupItem) {
+        if (String(lineupItem.fields.day_of_week) === "0") {
+            return lineupItem;
+        }
+    })
+    displayLineupItems(startDay);
+})
+
+//
+filterButton.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        removeActiveFromDaySelector();
+        btn.classList.add('day-select-active');
+        const dayId = e.currentTarget.dataset.id;
+        const lineupDay = lineup.filter(function (lineupItem) {
+            if (String(lineupItem.fields.day_of_week) === dayId) {
+                return lineupItem;
+            }
+        });
+        console.log(lineupDay);
+        displayLineupItems(lineupDay);
+    });
+});
+
+
+function removeActiveFromDaySelector() {
+    filterButtons.forEach((element) => {
+        element.classList.remove('day-select-active');
+    })
+}
+
+
+function displayLineupItems(lineupItems) {
+    let displayLineup = lineupItems.map(function (item) {
+        return `<li class="schedule-card">
+                <p class="schedule-card-title">${item.fields.artist_name}</p>
+                <p class="schedule-card-genre">${item.fields.genre}</p>
+                <p class="schedule-card-time">${item.fields.start_time} - ${item.fields.end_time}</p>
+            </li>`;
+    });
+    displayLineup = displayLineup.join("");
+    scheduleList.innerHTML = displayLineup;
+}
